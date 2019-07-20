28758598a518185eb13594ebe8838fef2533b6db
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-20 10:51:50 +0400

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hi: "changed-there" });
 });
 
 const PORT = process.env.PORT || 5000;
