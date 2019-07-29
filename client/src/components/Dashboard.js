575fa5ada3873b7493e480e582465f9cc6b3b403
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-30 00:41:35 +0400

    fix: spelling path name

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SurveyList from "./surveys/SurveyListSurveyList";
+import SurveyList from "./surveys/SurveyList/SurveyList";
 
 const Dashboard = () => {
   return (
